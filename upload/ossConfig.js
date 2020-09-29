1a86f5e69b510551c3bd7ff87eb086c795d58ae3
zhangxu <zhangxu@jiaaocap.com>
2020-09-29 10:02:02 +0800

    fix:bug修复

--- upload/ossConfig.js
+++ upload/ossConfig.js
@@ -1,8 +1,8 @@
 module.exports = Object.freeze({
   development: {//测试
     'region': 'oss-cn-hangzhou',
-    'accessKeyId': 'LTAI4G7n8rgf89BYmbTbfXqm',
-    'accessKeySecret': 'KkjSLtxCH2nF5tk3S94rLgbXYypmCH',
+    'accessKeyId': '',
+    'accessKeySecret': '',
     'localPath': './dist/**',
     'bucket': '',
     'ossPath': '/',
@@ -10,8 +10,8 @@ module.exports = Object.freeze({
   },
   production: {//正式
     'region': 'oss-cn-hangzhou',
-    'accessKeyId': 'LTAI4G5wYGUHTkgWREeoSrU1',
-    'accessKeySecret': 'doWmLj6v8IPnTNKY2W5J1MraE7e2Xh',
+    'accessKeyId': '',
+    'accessKeySecret': '',
     'localPath': './dist/**',
     'bucket': '',
     'ossPath': '/',
